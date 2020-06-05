375770965c0715f5c2885c89f3aa5592df7e74e3
Zuyang Kou <leafduo@gmail.com>
2020-06-05 17:50:21 +0800

    Add gops

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/leafduo/grpc-dev-proxy/handler"
 
+	"github.com/google/gops/agent"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +15,11 @@ var rootCmd = &cobra.Command{
 	Use:   "grpc-dev-proxy",
 	Short: "grpc-dev-proxy is a proxy for debugging gRPC service more easily",
 	Run: func(cmd *cobra.Command, args []string) {
+		if gops {
+			if err := agent.Listen(agent.Options{}); err != nil {
+				panic(err)
+			}
+		}
 		mux := http.NewServeMux()
 		mux.HandleFunc("/", handler.HandleRequest)
 
@@ -28,9 +34,11 @@ var rootCmd = &cobra.Command{
 }
 
 var port int
+var gops bool
 
 func init() {
 	rootCmd.PersistentFlags().IntVar(&port, "port", 2333, "listening port")
+	rootCmd.PersistentFlags().BoolVar(&gops, "gops", false, "enable gops")
 }
 
 func Execute() {
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.12
 
 require (
 	github.com/fullstorydev/grpcurl v1.2.1
+	github.com/google/gops v0.3.6
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jhump/protoreflect v1.2.0
 	github.com/sirupsen/logrus v1.3.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
-	golang.org/x/xerrors v0.0.0-20190513163551-3ee3066db522
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 	google.golang.org/grpc v1.19.0
 )
